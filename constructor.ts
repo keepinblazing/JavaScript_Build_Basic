6e9599f9e50c077bb1e0c5f592d102d7655c4005
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-10-16 22:18:52 +0900

    생성자 함수

---
+++ constructor.ts
@@ -0,0 +1,15 @@
+// 생성자 함수
+
+function Item(title: string, price: number) {
+  this.title = title;
+  this.price = price;
+  this.say = function () {
+    console.log(`가격은 ${price}원 입니다.`);
+  };
+}
+
+const item1 = new Item("나이키", 10000);
+const item2 = new Item("아디다스", 5000);
+const item3 = new Item("스투시", 3000);
+
+console.log(item1, item2, item3);
